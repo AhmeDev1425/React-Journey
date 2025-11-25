bf52d8babc9f27d1181a8b807e1499dbd0b9f584
Ahmed Hashim <devahmedswe@gmail.com>
2025-11-25 06:01:01 +0200

    refactor code form with usestate

--- project/src/MyForm.jsx
+++ project/src/MyForm.jsx
@@ -12,14 +12,22 @@ function MyForm(){
                     <label>Name: </label>
                     <input type="text"
                         value={getFormData.name}
-                        onChange={(e)=> {setFormData({name:e.target.value, email:getFormData.email})}}
+                        onChange={(e)=> {
+                            const newData = {...getFormData};
+                            newData.name = e.target.value;
+                            setFormData(newData);
+                        }}
                         />
 
                     <label>Email: </label>
 
                     <input type="text"
                         value={getFormData.email}
-                        onChange={(e)=> {setFormData({email:e.target.value, name:getFormData.name})}}
+                        onChange={(e)=> {
+                            const newData = {...getFormData};
+                            newData.email = e.target.value;
+                            setFormData(newData);
+                            }}
                         />
 
                 <button type="submit">submit</button>
